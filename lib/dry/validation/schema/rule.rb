d31aee6f69f30f855c79b28ce82925803f132487
Tim Cooper <coop@latrobest.com>
2016-03-29 17:42:10 +1000

    Add support for enum dry types

--- lib/dry/validation/schema/rule.rb
+++ lib/dry/validation/schema/rule.rb
@@ -110,7 +110,7 @@ module Dry
           node =
             if predicate.is_a?(::Symbol)
               [target.type, [name, [:predicate, [predicate, args]]]]
-            elsif predicate.is_a?(Types::Constrained)
+            elsif predicate.respond_to?(:rule)
               [target.type, [name, predicate.rule.to_ast]]
             else
               [target.type, [name, predicate.to_ast]]
--- spec/integration/schema/using_types_spec.rb
+++ spec/integration/schema/using_types_spec.rb
@@ -3,26 +3,29 @@ RSpec.describe Dry::Validation::Schema, 'defining schema using dry types' do
     Dry::Validation.Schema do
       key(:email).required(Email)
       key(:age).maybe(Age)
+      key(:country).required(Country)
     end
   end
 
   before do
     Email = Dry::Types['strict.string']
     Age = Dry::Types['strict.int'].constrained(gt: 18)
+    Country = Dry::Types['strict.string'].enum('Australia', 'Poland')
   end
 
   after do
     Object.send(:remove_const, :Email)
     Object.send(:remove_const, :Age)
+    Object.send(:remove_const, :Country)
   end
 
   it 'passes when input is valid' do
-    expect(schema.(email: 'jane@doe', age: 19)).to be_success
-    expect(schema.(email: 'jane@doe', age: nil)).to be_success
+    expect(schema.(email: 'jane@doe', age: 19, country: 'Australia')).to be_success
+    expect(schema.(email: 'jane@doe', age: nil, country: 'Poland')).to be_success
   end
 
   it 'fails when input is not valid' do
-    expect(schema.(email: '', age: 19)).to_not be_success
+    expect(schema.(email: '', age: 19, country: 'New Zealand')).to_not be_success
     expect(schema.(email: 'jane@doe', age: 17)).to_not be_success
     expect(schema.(email: 'jane@doe', age: '19')).to_not be_success
   end
