a9a88fa6eb50361e64c0738b3ae836132a5eee22
Piotr Solnica <piotr.solnica@gmail.com>
2016-03-30 14:52:10 +0200

    Add support for number? predicate

---
+++ spec/integration/schema/numbers_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe Dry::Validation::Schema do
+  subject(:schema) do
+    Dry::Validation.Schema do
+      key(:age).required(:number?, :int?)
+    end
+  end
+
+  it 'passes when value is a number and an int' do
+    expect(schema.(age: 132)).to be_success
+  end
+
+  it 'fails when value is not a number' do
+    expect(schema.(age: 'ops').messages).to eql(age: ['must be a number'])
+  end
+
+  it 'fails when value is not an integer' do
+    expect(schema.(age: 1.0).messages).to eql(age: ['must be an integer'])
+  end
+end
