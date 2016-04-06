fd88b82f24fbd8b4165a620d94f0596417318f69
Piotr Solnica <piotr.solnica@gmail.com>
2016-04-06 19:10:16 +0200

    Fix depending on deeply nested values in high-lvl rules

---
+++ spec/integration/schema/check_with_nested_el_spec.rb
@@ -0,0 +1,37 @@
+RSpec.describe 'Check depending on a nested value from a hash' do
+  subject(:schema) do
+    Dry::Validation.Schema do
+      key(:tag).schema do
+        key(:color).schema do
+          key(:value).required(:str?)
+        end
+      end
+
+      rule(red: [[:tag, :color, :value]]) do |value|
+        value.eql?('red')
+      end
+    end
+  end
+
+  it 'passes when check passes' do
+    expect(schema.(tag: { color: { value: 'red' }})).to be_success
+  end
+
+  it 'skips check when parent of the dependency failed' do
+    expect(schema.(tag: { color: :oops }).messages).to eql(
+      tag: { color: ['must be a hash'] }
+    )
+  end
+
+  it 'skips check when dependency failed' do
+    expect(schema.(tag: { color: { value: :oops }}).messages).to eql(
+      tag: { color: { value: ['must be a string'] } }
+    )
+  end
+
+  it 'fails when check fails' do
+    expect(schema.(tag: { color: { value: 'blue' }}).messages).to eql(
+      tag: { color: { value: ['must be equal to red'] } }
+    )
+  end
+end
