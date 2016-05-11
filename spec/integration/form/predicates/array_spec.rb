00fdfd1b5478ace4a877359d8c8fec28b772bfd9
Fran Worley <frances@safetytoolbox.co.uk>
2016-05-11 11:51:00 +0100

    fixes to @jodosha specs
    fixes a couple of things:
    
    - the array specs with_optional context blocks should mirror the
    required bock not test included_in.
    
    - lteq_test should be called lteq_spec

--- spec/integration/form/predicates/array_spec.rb
+++ spec/integration/form/predicates/array_spec.rb
@@ -74,12 +74,12 @@ RSpec.describe 'Predicates: Array' do
   context 'with optional' do
     subject(:schema) do
       Dry::Validation.Form do
-        optional(:foo) { included_in?(%w(1 3 5)) }
+        optional(:foo) { array? { each { int? } } }
       end
     end
 
     context 'with valid input' do
-      let(:input) { { 'foo' => '3' } }
+      let(:input) { { 'foo' => ['3'] } }
 
       it 'is successful' do
         expect(result).to be_successful
@@ -98,15 +98,15 @@ RSpec.describe 'Predicates: Array' do
       let(:input) { { 'foo' => nil } }
 
       it 'is not successful' do
-        expect(result).to be_failing ['must be one of: 1, 3, 5']
+        expect(result).to be_failing ['must be an array']
       end
     end
 
     context 'with blank input' do
       let(:input) { { 'foo' => '' } }
 
-      it 'is not successful' do
-        expect(result).to be_failing ['must be one of: 1, 3, 5']
+      it 'is successful' do
+        expect(result).to be_successful
       end
     end
 
@@ -114,15 +114,31 @@ RSpec.describe 'Predicates: Array' do
       let(:input) { { 'foo' => { 'a' => '1' } } }
 
       it 'is not successful' do
-        expect(result).to be_failing ['must be one of: 1, 3, 5']
+        expect(result).to be_failing ['must be an array']
       end
     end
 
-    context 'with invalid input' do
+    context 'with invalid input (integer)' do
       let(:input) { { 'foo' => '4' } }
 
       it 'is not successful' do
-        expect(result).to be_failing ['must be one of: 1, 3, 5']
+        expect(result).to be_failing ['must be an array']
+      end
+    end
+
+    context 'with invalid input (array with non-integers)' do
+      let(:input) { { 'foo' => %w(foo bar) } }
+
+      it 'is not successful' do
+        expect(result).to be_failing 0 => ['must be an integer'], 1 => ['must be an integer']
+      end
+    end
+
+    context 'with invalid input (mixed array)' do
+      let(:input) { { 'foo' => %w(1 bar) } }
+
+      it 'is not successful' do
+        expect(result).to be_failing 1 => ['must be an integer']
       end
     end
   end
