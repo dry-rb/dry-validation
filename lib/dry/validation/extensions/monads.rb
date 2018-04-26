c3b4fad314c40605485ba631e39f2bf136ed4731
Jean-Denis Koeck <jdkoeck@gmail.com>
2018-04-26 15:30:36 +0200

    Add option to return message_set as failure case
    In monads extension, useful for folks who need access to the
    underlying message_set

--- lib/dry/validation/extensions/monads.rb
+++ lib/dry/validation/extensions/monads.rb
@@ -5,11 +5,15 @@ module Dry
     class Result
       include Dry::Monads::Result::Mixin
 
-      def to_monad(options = EMPTY_HASH)
+      def to_monad(failure_value_as_message_set: false, **options)
         if success?
           Success(output)
         else
-          Failure(messages(options))
+          if failure_value_as_message_set
+            Failure(message_set(options))
+          else
+            Failure(messages(options))
+          end
         end
       end
       alias_method :to_either, :to_monad
