074a48a299494f810a724ad2698740425235b493
Piotr Solnica <piotr.solnica@gmail.com>
2019-03-04 12:03:25 +0100

    Add Result#[]= and Result#key?
    Closes #460
    Closes #461

---
+++ spec/integration/contract/evaluator/setting_values_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+RSpec.describe Dry::Validation::Evaluator, 'values writer' do
+  context 'when key does not exist' do
+    subject(:contract) do
+      Dry::Validation::Contract.build do
+        schema do
+          required(:email).filled(:string)
+          required(:user_id).filled(:integer)
+        end
+
+        rule(:user_id) do
+          if values[:user_id].equal?(312)
+            values[:user] = 'jane'
+          else
+            failure(:user, 'must be jane')
+          end
+        end
+
+        rule(:email) do
+          failure('is invalid') if values[:user] == 'jane' && values[:email] != 'jane@doe.org'
+        end
+      end
+    end
+
+    it 'stores new values between rule execution' do
+      expect(contract.(user_id: 3, email: 'john@doe.org').errors).to eql(user: ['must be jane'])
+      expect(contract.(user_id: 312, email: 'john@doe.org').errors).to eql(email: ['is invalid'])
+    end
+  end
+
+  context 'when key already exists' do
+    subject(:contract) do
+      Dry::Validation::Contract.build do
+        schema do
+          required(:email).filled(:string)
+        end
+
+        rule(:email) do
+          values[:email] = 'foo'
+        end
+      end
+    end
+
+    it 'raises error' do
+      expect { contract.(email: 'jane@doe.org') }.to raise_error(ArgumentError, /email/)
+    end
+  end
+end
