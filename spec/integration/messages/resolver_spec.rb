9deef22684aa5b262c751fa2295ede0d50f27296
Piotr Solnica <piotr.solnica@gmail.com>
2019-05-30 12:57:20 +0200

    Remove run-time locale option

--- spec/integration/messages/resolver_spec.rb
+++ spec/integration/messages/resolver_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe Dry::Validation::Messages::Resolver, '#message' do
   shared_context 'resolving' do
     subject(:resolver) do
-      contract_class.new(locale: locale, schema: proc {}).message_resolver
+      contract_class.new(schema: proc {}).message_resolver
     end
 
     let(:contract_class) do
@@ -21,22 +21,22 @@ RSpec.describe Dry::Validation::Messages::Resolver, '#message' do
       let(:locale) { :en }
 
       it 'returns message text for base rule' do
-        expect(resolver.message(:not_weekend, path: [nil]))
+        expect(resolver.message(:not_weekend, path: [nil], locale: locale))
           .to eql(['this only works on weekends', {}])
       end
 
       it 'returns message text for flat rule' do
-        expect(resolver.message(:taken, path: [:email], tokens: { email: 'jane@doe.org' }))
+        expect(resolver.message(:taken, path: [:email], tokens: { email: 'jane@doe.org' }, locale: locale))
           .to eql(['looks like jane@doe.org is taken', {}])
       end
 
       it 'returns message text for nested rule when it is defined under root' do
-        expect(resolver.message(:invalid, path: %i[address city]))
+        expect(resolver.message(:invalid, path: %i[address city], locale: locale))
           .to eql(['is not a valid city name', {}])
       end
 
       it 'returns message text for nested rule' do
-        expect(resolver.message(:invalid, path: %i[address street]))
+        expect(resolver.message(:invalid, path: %i[address street], locale: locale))
           .to eql(["doesn't look good", {}])
       end
 
@@ -52,22 +52,22 @@ RSpec.describe Dry::Validation::Messages::Resolver, '#message' do
       let(:locale) { :pl }
 
       it 'returns message text for base rule' do
-        expect(resolver.message(:not_weekend, path: [nil]))
+        expect(resolver.message(:not_weekend, path: [nil], locale: locale))
           .to eql(['to działa tylko w weekendy', {}])
       end
 
       it 'returns message text for flat rule' do
-        expect(resolver.message(:taken, path: [:email], tokens: { email: 'jane@doe.org' }))
+        expect(resolver.message(:taken, path: [:email], tokens: { email: 'jane@doe.org' }, locale: locale))
           .to eql(['wygląda, że jane@doe.org jest zajęty', {}])
       end
 
       it 'returns message text for nested rule when it is defined under root' do
-        expect(resolver.message(:invalid, path: %i[address city]))
+        expect(resolver.message(:invalid, path: %i[address city], locale: locale))
           .to eql(['nie jest poprawną nazwą miasta', {}])
       end
 
       it 'returns message text for nested rule' do
-        expect(resolver.message(:invalid, path: %i[address street]))
+        expect(resolver.message(:invalid, path: %i[address street], locale: locale))
           .to eql(['nie wygląda dobrze', {}])
       end
     end
