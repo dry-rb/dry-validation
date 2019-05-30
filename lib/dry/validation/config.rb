c063519ceb7ebbbe21dece55a4c2d0d9c2965f11
Piotr Solnica <piotr.solnica@gmail.com>
2019-05-30 10:48:17 +0200

    Simplify Config

--- lib/dry/validation/config.rb
+++ lib/dry/validation/config.rb
@@ -13,29 +13,6 @@ module Dry
     class Config < Schema::Config
       setting :locale, :en
       setting :macros, Macros::Container.new, &:dup
-
-      # @api private
-      def macros
-        config.macros
-      end
-
-      # @api private
-      def locale
-        config.locale
-      end
-
-      # @api private
-      def respond_to_missing?(meth, include_private = false)
-        super || config.respond_to?(meth, include_private)
-      end
-
-      private
-
-      # @api private
-      def method_missing(meth, *args, &block)
-        super unless config.respond_to?(meth)
-        config.public_send(meth, *args)
-      end
     end
   end
 end
