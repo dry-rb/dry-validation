4b77efa5ff6670fc6f0bff102d1c33af6c73cb2e
Piotr Solnica <piotr.solnica@gmail.com>
2019-06-06 21:17:43 +0200

    Change spec description

--- spec/integration/contract/class_interface/rule/each_spec.rb
+++ spec/integration/contract/class_interface/rule/each_spec.rb
@@ -5,23 +5,23 @@ require 'dry/validation/contract'
 RSpec.describe Dry::Validation::Contract, 'Rule#each' do
   subject(:contract) { contract_class.new }
 
-  let(:contract_class) do
-    Class.new(Dry::Validation::Contract) do
-      def self.name
-        'TestContract'
-      end
+  context 'using a block' do
+    let(:contract_class) do
+      Class.new(Dry::Validation::Contract) do
+        def self.name
+          'TestContract'
+        end
 
-      params do
-        required(:nums).array(:integer)
-      end
+        params do
+          required(:nums).array(:integer)
+        end
 
-      rule(:nums).each do
-        key.failure('invalid') if value < 3
+        rule(:nums).each do
+          key.failure('invalid') if value < 3
+        end
       end
     end
-  end
 
-  context 'when the value is an array' do
     it 'applies rule when an item passed schema checks' do
       expect(contract.(nums: ['oops', 1, 4, 0]).errors.to_h)
         .to eql(nums: { 0 => ['must be an integer'], 1 => ['invalid'], 3 => ['invalid'] })
