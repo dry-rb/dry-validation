755b41bde125b22047247e9f234888359b55a9d7
Piotr Solnica <piotr.solnica@gmail.com>
2019-06-06 21:19:09 +0200

    Support macros in Rule#each

--- spec/integration/contract/class_interface/rule/each_spec.rb
+++ spec/integration/contract/class_interface/rule/each_spec.rb
@@ -27,4 +27,30 @@ RSpec.describe Dry::Validation::Contract, 'Rule#each' do
         .to eql(nums: { 0 => ['must be an integer'], 1 => ['invalid'], 3 => ['invalid'] })
     end
   end
+
+  context 'using a macro' do
+    let(:contract_class) do
+      Class.new(Dry::Validation::Contract) do
+        def self.name
+          'TestContract'
+        end
+
+        register_macro(:min) do |macro:|
+          min = macro.args[0]
+          key.failure('invalid') if value < min
+        end
+
+        params do
+          required(:nums).array(:integer)
+        end
+
+        rule(:nums).each(min: 3)
+      end
+    end
+
+    it 'applies rule when an item passed schema checks' do
+      expect(contract.(nums: ['oops', 1, 4, 0]).errors.to_h)
+        .to eql(nums: { 0 => ['must be an integer'], 1 => ['invalid'], 3 => ['invalid'] })
+    end
+  end
 end
