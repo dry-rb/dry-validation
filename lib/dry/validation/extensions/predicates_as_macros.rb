b5f8f53e11111382111421af0976f4cfff9a8745
Marc Busqué <marc@lamarciana.com>
2019-06-27 10:27:41 +0200

    Extension to use dry-logic predicates as macros (#548)
    Closes #537

---
+++ lib/dry/validation/extensions/predicates_as_macros.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require 'dry/schema/predicate_registry'
+require 'dry/validation/contract'
+
+module Dry
+  module Validation
+    # Predicate registry with additional needed methods.
+    class PredicateRegistry < Schema::PredicateRegistry
+      # List of predicates to be imported by `:predicates_as_macros`
+      # extension.
+      #
+      # @see Dry::Validation::Contract
+      WHITELIST = %i[
+        filled? gt? gteq? included_in? includes? inclusion? is? lt?
+        lteq? max_size? min_size? not_eql? odd? respond_to? size? true?
+        uuid_v4?
+      ].freeze
+
+      # @api private
+      def arg_names(name)
+        arg_list(name).map(&:first)
+      end
+
+      # @api private
+      def call(name, args)
+        self[name].(*args)
+      end
+
+      # @api private
+      def message_opts(name, arg_values)
+        arg_names(name).zip(arg_values).to_h
+      end
+    end
+
+    # Extension to use dry-logic predicates as macros.
+    #
+    # @see Dry::Validation::PredicateRegistry::WHITELIST Available
+    # predicates
+    #
+    # @example
+    #   Dry::Validation.load_extensions(:predicates_as_macros)
+    #
+    #   class ApplicationContract < Dry::Validation::Contract
+    #     import_predicates_as_macros
+    #   end
+    #
+    #   class AgeContract < ApplicationContract
+    #     schema do
+    #       required(:name).filled(:integer)
+    #     end
+    #
+    #     rule(:age).validate(gteq?: 18)
+    #   end
+    #
+    #   AgeContract.new.(age: 17).errors.first.text
+    #   # => 'must be greater than or equal to 18'
+    class Contract
+      # Make macros available for self and its descendants.
+      def self.import_predicates_as_macros
+        registry = PredicateRegistry.new
+        PredicateRegistry::WHITELIST.each do |name|
+          register_macro(name) do |macro:|
+            predicate_args = [*macro.args, value]
+            key.failure(
+              name, registry.message_opts(name, predicate_args)
+            ) unless registry.(name, predicate_args)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/extensions/predicates_as_macros/contract_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'dry/validation/extensions/predicates_as_macros'
+
+RSpec.describe Dry::Validation::Contract do
+  context 'with predicates_as_macros extension' do
+    before { Dry::Validation.load_extensions(:predicates_as_macros) }
+
+    subject(:contract) do
+      Class.new(Dry::Validation::Contract) do
+        import_predicates_as_macros
+      end
+    end
+
+    let(:registry) { Dry::Validation::PredicateRegistry.new }
+
+    %i[
+      filled? gt? gteq? included_in? includes? inclusion? is? lt?
+      lteq? max_size? min_size? not_eql? odd? respond_to? size? true?
+      uuid_v4?
+    ].each do |predicate|
+      it "imports #{predicate}" do
+        expect(
+          registry.key?(predicate) && contract.macros.key?(predicate)
+        ).to be(true)
+      end
+    end
+
+    it 'macros succeed on predicate success' do
+      age_contract = Class.new(contract) do
+        schema do
+          required(:age).filled(:integer)
+        end
+
+        rule(:age).validate(gteq?: 18)
+      end.new
+
+      expect(age_contract.(age: 19)).to be_success
+    end
+
+    it 'macros fail on predicate failure' do
+      age_contract = Class.new(contract) do
+        schema do
+          required(:age).filled(:integer)
+        end
+
+        rule(:age).validate(gteq?: 18)
+      end.new
+
+      expect(age_contract.(age: 17)).to be_failure
+    end
+
+    it 'failure message is built from predicate name and arguments' do
+      age_contract = Class.new(contract) do
+        schema do
+          required(:age).filled(:integer)
+        end
+
+        rule(:age).validate(gteq?: 18)
+      end.new
+
+      result = age_contract.(age: 17)
+
+      expect(
+        result.errors.first.text
+      ).to eq('must be greater than or equal to 18')
+    end
+  end
+end
