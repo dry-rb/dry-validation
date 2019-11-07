8cdc4d682ea2fd491c1ca17bc57848aef01cf6e6
Nikita Shilnikov <fg@flashgordon.ru>
2019-11-07 17:25:25 +0300

    Set up coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,9 @@
 # frozen_string_literal: true
 
-if RUBY_ENGINE == 'ruby' && ENV['COVERAGE'] == 'true'
-  require 'yaml'
-  rubies = YAML.safe_load(File.read(File.join(__dir__, '..', '.travis.yml')))['rvm']
-  latest_mri = rubies.select { |v| v =~ /\A\d+\.\d+.\d+\z/ }.max
-
-  if RUBY_VERSION == latest_mri
-    require 'simplecov'
-    SimpleCov.start do
-      add_filter '/spec/'
-    end
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/spec/'
   end
 end
 
