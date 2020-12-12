8d09c253e81ba57927ef0e58e78938149cd262e2
Piotr Solnica <piotr.solnica@gmail.com>
2020-12-12 08:24:31 +0100

    rubocop -a --only Style/StringLiterals

--- spec/unit/rule_spec.rb
+++ spec/unit/rule_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-RSpec.describe Dry::Validation::Contract, '.rule' do
+RSpec.describe Dry::Validation::Contract, ".rule" do
   subject(:contract) { contract_class.new }
 
-  context 'with an array key' do
+  context "with an array key" do
     let(:contract_class) do
       Class.new(Dry::Validation::Contract) do
         schema do
@@ -14,27 +14,27 @@ RSpec.describe Dry::Validation::Contract, '.rule' do
       end
     end
 
-    it 'allows specifying a rule for array elements' do
+    it "allows specifying a rule for array elements" do
       contract_class.rule(:tags) do
-        key.failure('must have at least 1 element') unless value.size > 0
+        key.failure("must have at least 1 element") unless value.size > 0
       end
 
       expect(contract.(tags: []).errors.to_h).to eql(
-        tags: ['must have at least 1 element']
+        tags: ["must have at least 1 element"]
       )
     end
 
-    it 'shows the correct error message' do
+    it "shows the correct error message" do
       contract_class.rule(:tags) do
-        key.failure('must have at least 1 element') unless value.size > 0
+        key.failure("must have at least 1 element") unless value.size > 0
       end
 
-      expect(contract.(tags: [], name: '').errors.to_h).to eql(
-        tags: ['must have at least 1 element']
+      expect(contract.(tags: [], name: "").errors.to_h).to eql(
+        tags: ["must have at least 1 element"]
       )
 
-      expect(contract.(tags: [], name: '').errors(full: true).to_h).to eql(
-        tags: ['tags must have at least 1 element']
+      expect(contract.(tags: [], name: "").errors(full: true).to_h).to eql(
+        tags: ["tags must have at least 1 element"]
       )
     end
 
